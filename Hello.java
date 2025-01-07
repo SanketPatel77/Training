821bc38e41ac2f6157fbb02e8c59026ade0ef30b
SanketPatel77 <sanketpatel4872@gmail.com>
2025-01-07 14:00:52 +0530

    addition functionality added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 class Hello{
 	public static void main(String [] args){
 		System.out.println("Hello java");
+
+
+		//addition
+		int first = 10;
+		int second = 20;
+		System.out.println(first+second);
 	}
 }
